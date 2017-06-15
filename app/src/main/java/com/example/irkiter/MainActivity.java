2572180a81710708184b4adc72c96bde835b8f80
健太 <anpantti0720@gmail.com>
2017-06-15 14:46:37 +0900

    aaaa

--- app/src/main/java/com/example/irkiter/MainActivity.java
+++ app/src/main/java/com/example/irkiter/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (v == button) {
                     setupbtn();
+                    String a = "aa";
                 }
             }
         });
