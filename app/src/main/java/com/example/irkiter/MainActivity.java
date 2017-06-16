2b9edcd2c25776eb53bf8ef64dccd37954df4a99
tanma1210 <1401024@st.asojuku.ac.jp>
2017-06-16 15:00:25 +0900

    Rejected commit: Default

--- app/src/main/java/com/example/irkiter/MainActivity.java
+++ app/src/main/java/com/example/irkiter/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //セットアップボタン
+        //セットアップボタンつるだあ
         final Button button = (Button)findViewById(R.id.buttonSetup);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
