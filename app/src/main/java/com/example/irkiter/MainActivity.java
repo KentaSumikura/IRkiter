94da21cc7b11e0456d7a0db68e228566f063ec4f
tanma1210 <1401024@st.asojuku.ac.jp>
2017-06-16 15:24:47 +0900

    こみっとします

--- app/src/main/java/com/example/irkiter/MainActivity.java
+++ app/src/main/java/com/example/irkiter/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //つるだこうしん
+        //つるだこうしんあああ
 
         //セットアップボタンつるだあ
         final Button button = (Button)findViewById(R.id.buttonSetup);
